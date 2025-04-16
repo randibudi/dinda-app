353a73a859be81b650587d3837314ab22eaab48f
Randi Budi <randibudiprananda@proton.me>
2025-04-16 09:58:25 +0700

    feat(db): add learning materials schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, varchar, uuid, pgEnum } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  varchar,
+  uuid,
+  pgEnum,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 import { authUsers } from "drizzle-orm/supabase";
 
 export const userRole = pgEnum("user_role", ["admin", "student"]);
@@ -11,4 +18,13 @@ export const users = pgTable("users", {
   username: varchar("username", { length: 50 }).unique().notNull(),
   role: userRole("role").default("student"),
   grade: varchar("grade", { length: 10 }).default("IV"),
+});
+
+export const learningMaterials = pgTable("learning_materials", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: varchar("title", { length: 255 }).notNull(),
+  content: text("content").notNull(),
+  documentUrl: varchar("document_url", { length: 1024 }),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 });
