410d2d2d9688cbb4d93cbf0b650764c7069ca16a
Randi Budi <randibudiprananda@proton.me>
2025-05-01 23:02:23 +0700

    feat(navbar): add siswa navigation link

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -4,14 +4,17 @@
       class="mx-auto flex h-[65px] w-full max-w-[480px] items-center justify-around border-t border-(--ui-border-accented) bg-white"
     >
       <li>
+        <ULink to="/admin/siswa">Siswa</ULink>
+      </li>
+      <li>
         <ULink to="/admin/materi">Materi</ULink>
       </li>
       <li>
         <ULink to="/admin/quiz">Quiz</ULink>
       </li>
-      <li>Menu 3</li>
       <li>Menu 4</li>
       <li>Menu 5</li>
+      <li>Menu 6</li>
     </ul>
   </nav>
 </template>
