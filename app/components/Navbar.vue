04a102545e594d0a059da024bff886254fc8226e
Randi Budi <randibudiprananda@proton.me>
2025-05-06 07:12:46 +0700

    feat(navbar): add role-based navigation for admin and user views

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,20 +1,41 @@
+<script setup lang="ts">
+const user = useSupabaseUser();
+
+const isAdmin = computed(() => {
+  return user.value?.app_metadata?.role === "admin";
+});
+</script>
+
 <template>
   <nav class="sticky bottom-0 z-10">
     <ul
       class="mx-auto flex h-[65px] w-full max-w-[480px] items-center justify-around border-t border-(--ui-border-accented) bg-white"
     >
-      <li>
-        <ULink to="/admin/siswa">Siswa</ULink>
-      </li>
-      <li>
-        <ULink to="/admin/materi">Materi</ULink>
-      </li>
-      <li>
-        <ULink to="/admin/quiz">Quiz</ULink>
-      </li>
-      <li>
-        <ULink to="/diskusi">Diskusi</ULink>
-      </li>
+      <!-- Admin View -->
+      <template v-if="isAdmin">
+        <li>
+          <ULink to="/admin/siswa">Siswa</ULink>
+        </li>
+        <li>
+          <ULink to="/admin/materi">Materi</ULink>
+        </li>
+        <li>
+          <ULink to="/admin/quiz">Quiz</ULink>
+        </li>
+        <li>
+          <ULink to="/diskusi">Diskusi</ULink>
+        </li>
+      </template>
+
+      <!-- User View -->
+      <template v-else>
+        <li>
+          <ULink to="/">Beranda</ULink>
+        </li>
+        <li>
+          <ULink to="/diskusi">Diskusi</ULink>
+        </li>
+      </template>
     </ul>
   </nav>
 </template>
